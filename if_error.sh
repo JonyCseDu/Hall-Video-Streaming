9fbbdf64d7f4fd3e9c11b243158f85023d1624c3
JonyCseDu <jony.du.12.12.12@gmail.com>
2015-10-19 17:07:40 +0600

    mahmud ok

--- if_error.sh
+++ if_error.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 git fetch --all
-git reset --hard origin/master
+
+# note the SHA-1 of latest commit
+$ git checkout master
+# reset your branch head to your previously detached commit
+$ git reset --hard
+
+git config --global push.default matching
+git config --global push.default simple
+
+email="email"
+name="name"
+
+read -p "Email : " email
+read -p "Name : " name
+
+git config --global user.email "$email"
+gut config --global user.name "$name"
 
 echo "DONE"
 
